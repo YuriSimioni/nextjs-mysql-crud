b2e65ae65537db5c4ac99e7cf6c5ada64e965050
YuriSimioni <yurirs.contato@gmail.com>
2026-07-29 21:32:31 -0300

    ✨ chore: Disable devIndicator Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  devIndicators: false,
+  devIndicators: false, /* Disable devIcon in page */
 };
 
 export default nextConfig;
